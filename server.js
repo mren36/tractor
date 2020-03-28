1b8adf82d98982ddf04e9539044109224b3b2167
Michael Ren <mren36@github.com>
2020-03-28 13:51:09 -0400

    update server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var http = require("http");
 var path = require("path");
+const port = process.env.PORT || 5000;
 var socketIO = require("socket.io");
 var app = express();
 var server = http.Server(app);
@@ -9,12 +10,12 @@ var io = socketIO(server, {
   pingInterval: 2000,
   pingTimeout: 300000
 });
-app.set("port", 5000);
+app.set("port", port);
 app.use("/static", express.static(__dirname + "/static")); // Routing
 app.get("/", function(request, response) {
   response.sendFile(path.join(__dirname, "index.html"));
 }); // Starts the server.
-server.listen(5000, function() {
+server.listen(port, function() {
   console.log("Starting server on port 5000");
 });
 
